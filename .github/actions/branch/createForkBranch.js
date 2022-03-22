a0bc45f715114059ca88dc721ccb139be2cb5bc5
Daniel Lu <dl1644@gmail.com>
2022-03-21 17:34:58 -0700

    updating branch if it already exists

--- .github/actions/branch/createForkBranch.js
+++ .github/actions/branch/createForkBranch.js
@@ -2,64 +2,44 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 
 run();
-
 async function run() {
   const octokit = github.getOctokit(process.env.GITHUB_TOKEN);
   const context = github.context;
   try {
-    // Need to grab PR number since the event won't have the sha/branch name available
-    console.log(`context: ${JSON.stringify(context, undefined, 2)}`);
-    let prNumber = github.context.issue.number;
-    console.log('prNumber', prNumber)
-    let prInfo = await octokit.rest.pulls.get({
+    // Get info of the fork PR
+    const prNumber = context.issue.number;
+    const {data} = await octokit.rest.pulls.get({
       ...context.repo,
       pull_number: prNumber
     });
-    console.log(`pr info: ${JSON.stringify(prInfo, undefined, 2)}`);
+    const branch = `${data.user.login}-${data.head.ref}`;
+    const ref = `refs/heads/${branch}`;
+    const sha = data.head.sha;
 
-    // let branch = core.getInput('branch');
-    // let sha = core.getInput('sha');
-    // console.log(`branch: ${github.context.branch} and sha: ${github.context.sha}`)
-    // core.debug(`branch: ${branch} and sha: ${sha}`);
+    // Look up if branch for fork PR exists in base repo
+    let res = await octokit.rest.repos.getBranch({
+      ...context.repo,
+      branch
+    });
 
-    // branch = branch.replace('refs/heads/', '');
-    // let ref = `refs/heads/${branch}`;
-    // core.debug(`ref: ${ref}`);
-    // Testing if there is a PR related to the commit
-    // let {data: prs} = await octokit.rest.repos.listPullRequestsAssociatedWithCommit({
-    //   ...github.context.repo,
-    //   commit_sha: github.context.sha
-    // });
-
-    // core.debug(`prs: ${prs}`)
-
-    // TODO: replace hardcoded branch and ref
-    let branch = 'blah';
-    let ref = 'refs/heads/blah';
-    let sha;
-
-    try {
-      await octokit.rest.repos.getBranch({
-        ...github.context.repo,
-        branch
+    if (res.status === 200) {
+      // If branch already exists update it to match fork PR state
+      await octokit.rest.git.updateRef({
+        ...context.repo,
+        sha,
+        ref,
+        force: true
+      })
+    } else if (res.status === 404) {
+      // If branch doesn't exist for the forked PR, create one so we can get a
+      // build for it
+      await octokit.rest.git.createRef({
+        ...context.repo,
+        ref,
+        sha: sha
       });
-    } catch (error) {
-      if (error.name === 'HttpError' && error.status === 404) {
-        console.log('in if');
-        // core.debug('in if')
-        const resp = await octokit.rest.git.createRef({
-          ref,
-          sha: sha || context.sha,
-          ...context.repo,
-        });
-        console.log(`response: ${resp}`);
-        // core.debug(`response: ${resp.data.ref}`);
-        // return resp?.data?.ref === ref;
-      } else {
-        // If
-        throw Error(error);
-      }
     }
+
   } catch (error) {
     core.setFailed(error.message);
   }
