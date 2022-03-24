89547b800663ec447f825a0306f60514ffb500d3
Daniel Lu <dl1644@gmail.com>
2022-03-24 11:31:19 -0700

    debugging resource issue

--- .github/actions/branch/createForkBranch.js
+++ .github/actions/branch/createForkBranch.js
@@ -12,25 +12,28 @@ async function run() {
       ...context.repo,
       pull_number: prNumber
     });
-
+    console.log('1')
     if (data.head.repo.full_name !== context.payload.repository.full_name) {
       const branch = `${data.user.login}-${data.head.ref}`;
       const ref = `refs/heads/${branch}`;
       const sha = data.merge_commit_sha;
-
+      console.log('branch', branch, ref, sha)
       let res;
       // Look up if branch for fork PR exists in base repo
       try {
+        console.log('2')
         res = await octokit.rest.repos.getBranch({
           ...context.repo,
           branch
         });
       } catch (error) {
         if (!(error.name === 'HttpError' && error.status === 404)) {
+          console.log('3')
           throw error;
         } else {
           // If branch doesn't exist for the forked PR, create one so we can get a
           // build for it and return
+          console.log('4')
           await octokit.rest.git.createRef({
             ...context.repo,
             ref,
@@ -42,6 +45,7 @@ async function run() {
 
       // If branch already exists update it to match fork PR state.
       if (res.status === 200) {
+        console.log('5')
         await octokit.rest.git.updateRef({
           ...context.repo,
           sha,
@@ -51,6 +55,7 @@ async function run() {
       }
     }
   } catch (error) {
+    console.log('error', error)
     core.setFailed(error.message);
   }
 }
